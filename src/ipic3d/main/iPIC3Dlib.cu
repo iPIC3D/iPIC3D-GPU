485c0d173c32ab3d91f043c5da335d62f16b1200
Ivy Peng <56741918+IBPeng@users.noreply.github.com>
2025-03-22 09:03:08 -0700

    update timing

--- src/ipic3d/main/iPIC3Dlib.cu
+++ src/ipic3d/main/iPIC3Dlib.cu
@@ -170,6 +170,11 @@ int c_Solver::Init(int argc, char **argv) {
     col->Print();
     col->save();
   }
+
+  
+  my_clock = new Timing(myrank);
+
+  
   // Create the local grid
   grid = new Grid3DCU(col, vct);  // Create the local grid
   EMf = new EMfields3D(col, grid, vct);  // Create Electromagnetic Fields Object
@@ -313,8 +318,6 @@ int c_Solver::Init(int argc, char **argv) {
 
 
   initCUDA();
-
-  my_clock = new Timing(myrank);
 
   return 0;
 }
